647da3ebc854539b74f1a06214cb327b0a9cee22
ctroncosoimed <ctroncoso@i-med.cl>
2019-04-11 15:53:10 -0400

    1.0.6

--- scripts/dameunmanuel.js
+++ scripts/dameunmanuel.js
@@ -17,10 +17,13 @@ const images = [
   'https://www.maray.cl/wp-content/uploads/2018/05/marco-antonio-solis.jpg',
   'http://www.htv.com/wp-content/uploads/2016/09/SOLIS-759x500.jpg',
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1C3q7TUUiIC8tsrfUc7D2m3RtIBc3IkcaUNwRjTjJ9-JNFxDfFQ',
-  'https://media.metrolatam.com/2018/12/16/46312933101610486826002975542087621070553088n-6ae634d3c67cd8947ce82343ab4996cb-1200x600.jpg',
   'https://sss.lainolvidable.com.pe/imagen/apaisado/historia-cancion-si-no-te-hubieras-ido-marco-antonio-solis-ff7e7.jpg',
   'https://www.eluniversal.com.mx/sites/default/files/styles/f03-651x400/public/2018/12/20/marco_anronio_solis_hospitalizado.jpg?itok=T6pPAo0k',
-  'http://static.t13.cl/images/sizes/1200x675/1551326069-auno1046822.jpg'
+  'http://static.t13.cl/images/sizes/1200x675/1551326069-auno1046822.jpg',
+  'https://www.abc.es/media/estilo/2019/03/13/maluma-khZG--620x349@abc.JPG',
+  'https://assets.trome.pe/files/ec_article_multimedia_gallery/uploads/2019/01/18/5c4203c3e6662.jpeg',
+  'https://www.buenamusica.com/media/fotos/noticias/maluma/maluma-nombre-significado.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQccNQcXXdV6tzXoKK6ruWhSsMwgHJqZZLt4iGG7T4XBz5Vedhr'
 ]
 
 module.exports = robot => {
