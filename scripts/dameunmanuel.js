0d83feddafcbe0452358c62704da1848332d0f44
pcalderonimed <pcalderon@i-med.cl>
2019-04-15 16:46:05 -0400

    modificacion script dameunmanue.js

--- scripts/dameunmanuel.js
+++ scripts/dameunmanuel.js
@@ -23,7 +23,13 @@ const images = [
   'https://www.abc.es/media/estilo/2019/03/13/maluma-khZG--620x349@abc.JPG',
   'https://assets.trome.pe/files/ec_article_multimedia_gallery/uploads/2019/01/18/5c4203c3e6662.jpeg',
   'https://www.buenamusica.com/media/fotos/noticias/maluma/maluma-nombre-significado.jpg',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQccNQcXXdV6tzXoKK6ruWhSsMwgHJqZZLt4iGG7T4XBz5Vedhr'
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQccNQcXXdV6tzXoKK6ruWhSsMwgHJqZZLt4iGG7T4XBz5Vedhr',
+  'http://static.t13.cl/images/sizes/1200x675/1535831092-063951724586.jpg',
+  'https://www.elprogreso.es/media/elprogreso/images/2018/10/22/2018102211443027167.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSerGR-SAzw8vfmamabhvffLDcSVPPIUgnJoHrsnOWVX9RnlkLQ',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7gJrRIbBPg8_rT0XPQxWzkvk9laOEvEpiTIVSxzAfwLexJkPM',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8y7ZsCFisfJ_TSLg7OO9pxn_R0fM_blbaMcIjjRE5GomKKUm7',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGLeWlBcCjipdekAQ6EkLgGmrzU02z4KK1SsYOUN06zmIbmDQ7UQ'
 ]
 
 module.exports = robot => {
