3dad0ffcb0e61b655de5fc90679d84782e2cd7f6
pcalderonimed <pcalderon@i-med.cl>
2019-04-17 10:34:58 -0400

    dame un roa

---
+++ scripts/dameunroa.js
@@ -0,0 +1,25 @@
+// Description:
+//   Muestra una foto de luis roa de forma random
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Command:
+//   huemul dame un keldor
+//
+// Author:
+//   @pcalderon
+
+const images = [
+    'https://pbs.twimg.com/profile_images/547879613951516677/JfzEcf_B.jpeg',
+    'https://i.ytimg.com/vi/WZPLqomme9Y/hqdefault.jpg',
+    'https://i.ytimg.com/vi/OW4NeGVqzbE/maxresdefault.jpg'
+  ]
+  
+  module.exports = robot => {
+    robot.respond(/dame un roa/gi, msg => msg.send(msg.random(images)))
+  }
+  
